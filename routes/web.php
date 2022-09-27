6eeadfad649b8952aeefaa9de6460f3cad8ff189
Sylvio Cavalcanti <sylviocavalcanti2020@gmail.com>
2022-09-27 14:04:38 -0300

    Definiando prefixo e agrupando rotas

--- routes/web.php
+++ routes/web.php
@@ -15,40 +15,12 @@ use Illuminate\Support\Facades\Route;
 
 
 Route::get('/', 'PrincipalController@principal');
-
 Route::get('/sobre-nos', 'SobreNosController@sobreNos');
-
 Route::get('/contato', 'ContatoController@contato');
 
-Route::get('/login', fn() => 'Login');// PHP arrow function
+Route::prefix('/app')->group(function(){
+    Route::get('/login', fn() => 'Login');
+    Route::get('/clientes', fn() => 'Clientes');
+    Route::get('/produtos', fn() => 'Produtos');
+});
 
-Route::get('/clientes', fn() => 'Clientes');
-
-Route::get('/produtos', fn() => 'Produtos');
-
-
-// 'categoria' na pratica seria um select com as possibilidades
-// Route::get(
-//     '/contato/{nome}/{categoria_id}', 
-//     function (
-//         string $nome = 'Desconhecido', 
-//         int $categoria_id = 1 // O default é um inteiro. 1 - 'Informação' 
-//     ) {
-//         echo "Meu nome é: $nome - $categoria_id";
-//     }
-// )->where('categoria_id', '[0-9]+')->where('nome', '[A-Za-z]+'); 
-// Primeiro Where: pelo menos um 1, valor númerico 
-// Segundo Where: pelo menos um caractere de A - Z maiusculo ou minusculo
-
-
-/*
-VERBOS HTTP:
-
-get
-post
-put
-patch
-delete
-options
-
-*/
