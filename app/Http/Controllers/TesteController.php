1e0cdbca83a9728823cb1bfd5936e4ec481ca9ab
Sylvio Cavalcanti <sylviocavalcanti2020@gmail.com>
2022-09-27 17:40:12 -0300

    Recebendo parâmetros na routes e encaminhando para os controllers nas actions

---
+++ app/Http/Controllers/TesteController.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class TesteController extends Controller
+{
+    public function teste(int $p1, int $p2){ // Parâmetros tipados como inteiros
+        echo"A soma de $p1 + $p2 é: ".($p1+$p2); // String concatenada
+    }
+}
