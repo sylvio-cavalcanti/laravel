c6270c3f9642a81f99d08bfe3e925249d1dd9949
Sylvio Cavalcanti <sylviocavalcanti2020@gmail.com>
2022-09-27 16:27:21 -0300

    Redirecionamento de rotas

--- routes/web.php
+++ routes/web.php
@@ -25,3 +25,12 @@ Route::prefix('/app')->group(function(){
     Route::get('/produtos', fn() => 'Produtos')->name('app.produtos');
 });
 
+Route::get('/rota1', function() {
+    echo'Rota 1';
+})->name('site.rota1');
+
+Route::get('/rota2', function() {
+    return redirect()->route('site.rota1');
+})->name('site.rota2');
+
+// Route::redirect('/rota2', 'rota1');
