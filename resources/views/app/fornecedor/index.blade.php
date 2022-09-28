4107808ac41902f8677e697f0643c5afd684b1df
Sylvio Cavalcanti <sylviocavalcanti2020@gmail.com>
2022-09-28 18:17:37 -0300

    Exemplo de uso: @if / @elseif / @else

--- resources/views/app/fornecedor/index.blade.php
+++ resources/views/app/fornecedor/index.blade.php
@@ -1,10 +1,9 @@
 <h3>Fornecedor.</h3>
 
-{{-- Isso é um comentário --}}
-
-@php
-    // Comentario na sintaxe php
-@endphp
-
-{{ 'Texto de teste' }} 
-<?= 'Texto de teste' ?>
+@if(count($fornecedores) > 0 && count($fornecedores) < 10)
+    <h3>Existem alguns fornecedores cadastrados. </h3>
+@elseif(count($fornecedores) > 10)
+    <h3>Existem vários fornecedores cadastrados. </h3>
+@else 
+    <h3>Ainda não existem fornecedores cadastrados.</h3>
+@endif
