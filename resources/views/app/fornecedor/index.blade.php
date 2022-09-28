95a15c38c613239c3178d78b77d1068f0377869b
Sylvio Cavalcanti <sylviocavalcanti2020@gmail.com>
2022-09-28 18:41:54 -0300

    Exemplo de uso: @unless

--- resources/views/app/fornecedor/index.blade.php
+++ resources/views/app/fornecedor/index.blade.php
@@ -1,9 +1,14 @@
 <h3>Fornecedor.</h3>
+{{-- @dd($fornecedores) --}}
 
-@if(count($fornecedores) > 0 && count($fornecedores) < 10)
-    <h3>Existem alguns fornecedores cadastrados. </h3>
-@elseif(count($fornecedores) > 10)
-    <h3>Existem vários fornecedores cadastrados. </h3>
-@else 
-    <h3>Ainda não existem fornecedores cadastrados.</h3>
+Fornecedor: {{ $fornecedores[0]['nome'] }}
+<br>
+Status: {{ $fornecedores[0]['status'] }}
+<br>
+@if ( !($fornecedores[0]['status'] == 'S') ) <!-- IF Executa se o retorno da condição for TRUE --> 
+    Fornecedor inativo.
 @endif
+<br>
+@unless ($fornecedores[0]['status'] == 'S') <!-- UNLESS  Executa se o retorno da condição for FALSE --> 
+    Fornecedor inativo.
+@endunless
