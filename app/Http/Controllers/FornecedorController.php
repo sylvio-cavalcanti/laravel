8cb71c2d997a3d76b950040ec2f3164ef84868f0
Sylvio Cavalcanti <sylviocavalcanti2020@gmail.com>
2022-09-29 11:37:46 -0300

    Operadores ternários

--- app/Http/Controllers/FornecedorController.php
+++ app/Http/Controllers/FornecedorController.php
@@ -10,13 +10,15 @@ class FornecedorController extends Controller
         $fornecedores = [
             0 =>[  'nome' => 'Fornecedor 1', 
                     'status' => 'N',
-                    'cnpj' => ''
+                    'cnpj' => '5844'
                 ],
             1 =>[  
                 'nome' => 'Fornecedor 1', 
                 'status' => 'S'
             ]
         ];
+        echo isset($fornecedores[0]['cnpj']) ? 'CNPJ informado' : 'CNPJ não informado.';
+
         return view('app.fornecedor.index', compact('fornecedores'));
     }
 }
