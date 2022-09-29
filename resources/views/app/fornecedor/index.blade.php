cc0c1698172b2f80c892705924ee59d677dff8ee
Sylvio Cavalcanti <sylviocavalcanti2020@gmail.com>
2022-09-29 16:34:41 -0300

    @forelse

--- resources/views/app/fornecedor/index.blade.php
+++ resources/views/app/fornecedor/index.blade.php
@@ -1,7 +1,7 @@
 <h3>Fornecedor.</h3>
 
 @isset($fornecedores)
-    @foreach($fornecedores as $indice => $fornecedor)
+    @forelse($fornecedores as $indice => $fornecedor)
         Fornecedor: {{ $fornecedor['nome'] }}
         <br>
         Status: {{ $fornecedor['status'] }}
@@ -11,5 +11,8 @@
         Telefone: ({{ $fornecedor['ddd'] ?? '' }}) {{ $fornecedor['telefone'] ?? '' }}
         <hr>
         <br>
-    @endforeach
+    @empty 
+        <!-- Caso a variavel esteja vazia, o fluxo do loop é desviado para o bloco do 'empty' -->
+        Não existem fornecedores cadastrados.
+    @endforelse
 @endisset
