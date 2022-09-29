b843e76678a2a0c4cf8d5c10afd70567091e7443
Sylvio Cavalcanti <sylviocavalcanti2020@gmail.com>
2022-09-29 16:23:35 -0300

    @foreach()

--- resources/views/app/fornecedor/index.blade.php
+++ resources/views/app/fornecedor/index.blade.php
@@ -1,15 +1,15 @@
 <h3>Fornecedor.</h3>
 
 @isset($fornecedores)
-    @for($i = 0; isset($fornecedores[$i]); $i++)
-        Fornecedor: {{ $fornecedores[$i]['nome'] }}
+    @foreach($fornecedores as $indice => $fornecedor)
+        Fornecedor: {{ $fornecedor['nome'] }}
         <br>
-        Status: {{ $fornecedores[$i]['status'] }}
+        Status: {{ $fornecedor['status'] }}
         <br>
-        CNPJ: {{  $fornecedores[$i]['cnpj'] ?? 'Não informado.' }}
+        CNPJ: {{  $fornecedor['cnpj'] ?? 'Não informado.' }}
         <br>
-        Telefone: ({{ $fornecedores[$i]['ddd'] ?? '' }}) {{ $fornecedores[$i]['telefone'] ?? '' }}
+        Telefone: ({{ $fornecedor['ddd'] ?? '' }}) {{ $fornecedor['telefone'] ?? '' }}
+        <hr>
         <br>
-        <br>
-    @endfor
+    @endforeach
 @endisset
