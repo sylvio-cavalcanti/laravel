463f82d16778a681bb5a9ae618898e55316bd4c3
Sylvio Cavalcanti <sylviocavalcanti2020@gmail.com>
2022-09-29 15:33:07 -0300

    @switch

--- app/Http/Controllers/FornecedorController.php
+++ app/Http/Controllers/FornecedorController.php
@@ -10,11 +10,23 @@ class FornecedorController extends Controller
         $fornecedores = [
             0 =>[  'nome' => 'Fornecedor 1', 
                     'status' => 'N',
-                    'cnpj' => null
+                    'cnpj' => null,
+                    'ddd' => '11', // São Paulo (SP)
+                    'telefone' => '0000-0000'
                 ],
             1 =>[  
-                'nome' => 'Fornecedor 1', 
-                'status' => 'S'
+                'nome' => 'Fornecedor 2', 
+                'status' => 'S',
+                'cnpj' => null,
+                'ddd' => '85', // Fortaleza (CE)
+                'telefone' => '0000-0000'
+            ],
+            2 =>[  
+                'nome' => 'Fornecedor 3', 
+                'status' => 'S',
+                'cnpj' => null,
+                'ddd' => '32', // Juiz de Fora (MG)
+                'telefone' => '0000-0000'
             ]
         ];
         return view('app.fornecedor.index', compact('fornecedores'));
--- resources/views/app/fornecedor/index.blade.php
+++ resources/views/app/fornecedor/index.blade.php
@@ -4,10 +4,26 @@
    
 @endphp
 
-@isset($fornecedores) <!-- So entra nesse bloco de codigo, se a variavel existe --> 
+@isset($fornecedores) 
     Fornecedor: {{ $fornecedores[0]['nome'] }}
     <br>
     Status: {{ $fornecedores[0]['status'] }}
     <br>
     CNPJ: {{  $fornecedores[0]['cnpj'] ?? 'Não informado.' }}
+    <br>
+    Telefone: ({{ $fornecedores[0]['ddd'] ?? '' }}) {{ $fornecedores[0]['telefone'] ?? '' }}
+    @switch($fornecedores[0]['ddd'])
+        @case('11')
+            São Paulo - SP
+            @break
+        @case('32')
+            Juiz de Fora - MG
+            @break
+        @case('85')
+            Fortaleza - CE
+            @break
+        @default
+            Estado não identificado.
+            @break
+    @endswitch
 @endisset
