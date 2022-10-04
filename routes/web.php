9d4631e4398a1aa103d67d6fb8dc69114f2c92b7
Sylvio Cavalcanti <sylviocavalcanti2020@gmail.com>
2022-10-04 18:11:21 -0300

    Eviando formulario de contato utilizando POST

--- routes/web.php
+++ routes/web.php
@@ -17,6 +17,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', 'PrincipalController@principal')->name('site.index');
 Route::get('/sobre-nos', 'SobreNosController@sobreNos')->name('site.sobrenos');
 Route::get('/contato', 'ContatoController@contato')->name('site.contato');
+Route::post('/contato', 'ContatoController@contato')->name('site.contato');
 Route::get('/login', fn() => 'Login')->name('site.login');
 
 Route::prefix('/app')->group(function(){ 
