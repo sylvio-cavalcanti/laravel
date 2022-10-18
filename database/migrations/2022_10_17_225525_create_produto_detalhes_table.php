5dbd330776975b520e94ae4723f60e143f3e18ae
Sylvio Cavalcanti <sylviocavalcanti2020@gmail.com>
2022-10-17 21:23:16 -0300

    Criando migration com constraints para garantir um relacionamento 1x1 entre tabelas

---
+++ database/migrations/2022_10_17_225525_create_produto_detalhes_table.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateProdutoDetalhesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('produto_detalhes', function (Blueprint $table) {
+            $table->id();
+            // Padronização que é automaticamente identificada pelo Laravel:
+            // singular do nome da tabela + _id = 'produto_id'
+            // Colunas
+            $table->unsignedBigInteger('produto_id');
+            $table->float('comprimento', 8, 2);
+            $table->float('largura', 8, 2);
+            $table->float('altura', 8, 2);
+            $table->timestamps();
+
+            // Constraint
+            $table->foreign('produto_id')->references('id')->on('produtos');
+            $table->unique('produto_id');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('produto_detalhes');
+    }
+}
