777ca99ab7044e8bafe143ef16dde3f36938ca06
Sylvio Cavalcanti <sylviocavalcanti2020@gmail.com>
2022-11-04 10:27:59 -0300

    Criando seeder: FornecedorSeeder

---
+++ database/seeds/FornecedorSeeder.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use App\Fornecedor; // Trazendo a classe Fornecedor para o contexto
+use \Illuminate\Support\Facades\DB; // Para utilizar no método insert
+
+class FornecedorSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        // Instanciando o objeto
+        $fornecedor = new Fornecedor();
+        $fornecedor->nome = 'Fornecedor 100';
+        $fornecedor->site = 'fornecedor100.com.br';
+        $fornecedor->uf = 'CE';
+        $fornecedor->email = 'contato@fornecedor100.com.br';
+        $fornecedor->save();
+        
+        // Utilizando o método create(). Atenção para o atributo fillable da classe
+        Fornecedor::create([
+            'nome' => 'Fornecedor 200',
+            'site' => 'fornecedor200.com.br',
+            'uf' => 'RS',
+            'email' => 'contato@fornecedor200.com.br'
+        ]);
+
+        // Utilizando o método insert()
+        DB::table('fornecedores')->insert([
+            'nome' => 'Fornecedor 300',
+            'site' => 'fornecedor300.com.br',
+            'uf' => 'SP',
+            'email' => 'contato@fornecedor300.com.br'
+        ]);
+    }
+}
