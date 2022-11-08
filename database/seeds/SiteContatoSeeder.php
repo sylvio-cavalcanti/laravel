96d4199c453b01e3c426fa5d03d0751dd8e5538c
Sylvio Cavalcanti <sylviocavalcanti2020@gmail.com>
2022-11-08 12:55:02 -0300

    Criando uma Factory para gerar registros na tabela site_contato

--- database/seeds/SiteContatoSeeder.php
+++ database/seeds/SiteContatoSeeder.php
@@ -12,12 +12,14 @@ class SiteContatoSeeder extends Seeder
      */
     public function run()
     {
-        $contato = new SiteContato();
-        $contato->nome = 'Sistema SG';
-        $contato->telefone = '(11)99999-8888';
-        $contato->email = 'contato@sg.com.br';
-        $contato->motivo_contato = 1;
-        $contato->mensagem = 'Seja bem-vindo ao sistema Super Gestão.';
-        $contato->save(); // Salvabdo registro na tabela site_contatos
+        // $contato = new SiteContato();
+        // $contato->nome = 'Sistema SG';
+        // $contato->telefone = '(11)99999-8888';
+        // $contato->email = 'contato@sg.com.br';
+        // $contato->motivo_contato = 1;
+        // $contato->mensagem = 'Seja bem-vindo ao sistema Super Gestão.';
+        // $contato->save(); // Salvabdo registro na tabela site_contatos
+
+        factory(SiteContato::class, 100)->create();
     }
 }
