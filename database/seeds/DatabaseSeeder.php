57da777d23522966e519ec1bcb593575b96d3a0d
Sylvio Cavalcanti <sylviocavalcanti2020@gmail.com>
2022-11-08 11:22:42 -0300

    Criando seeder para tabela site_contatos

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -13,5 +13,7 @@ class DatabaseSeeder extends Seeder
     {
         // $this->call(UsersTableSeeder::class);
         $this->call(FornecedorSeeder::class);
+        $this->call(SiteContatoSeeder::class);
+
     }
 }
