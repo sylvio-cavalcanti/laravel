7a8febe44f9857fb7742a3e228d671b9c311ab0b
Sylvio Cavalcanti <sylviocavalcanti2020@gmail.com>
2022-11-10 13:21:45 -0300

    Gravando os dados do formulario no banco de dados utilizando os metodos fill() e create()

--- app/Http/Controllers/ContatoController.php
+++ app/Http/Controllers/ContatoController.php
@@ -3,17 +3,29 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\SiteContato;
 
 class ContatoController extends Controller
 {
     public function contato(Request $request) {
-        echo '<pre>';
-            print_r($request->all());
-        echo '</pre>';
-        echo $request->input('nome');
-        echo '<br>';
-        echo $request->input('email');
-        echo '<br>';
-        return view('site.contato', ['titulo' => 'Contato']); // Array associativo
+
+        // $contato = new SiteContato();
+        // $contato->nome = $request->input('nome');
+        // $contato->telefone = $request->input('telefone');
+        // $contato->email = $request->input('email');
+        // $contato->motivo_contato = $request->input('motivo_contato');
+        // $contato->mensagem = $request->input('mensagem');
+        // // print_r($contato->getAttributes());
+        // $contato->save();
+        
+        // $contato = new SiteContato();
+        // $contato->fill($request->all());
+        // print_r($contato->getAttributes());
+        // $contato->save();
+
+        $contato = new SiteContato();
+        $contato->create($request->all());
+
+        return view('site.contato', ['titulo' => 'Contato (teste)']);
     }
 }
