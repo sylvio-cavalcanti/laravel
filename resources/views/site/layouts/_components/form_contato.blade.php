937e401b0464dff9feceb5f695ec223b086bf571
Sylvio Cavalcanti <sylviocavalcanti2020@gmail.com>
2023-01-05 12:26:48 -0300

    Metodo old(): preenchendo forms novamente quando houver um erro de validação

--- resources/views/site/layouts/_components/form_contato.blade.php
+++ resources/views/site/layouts/_components/form_contato.blade.php
@@ -1,11 +1,11 @@
 {{ $slot }}
 <form action={{route('site.contato')}} method="post"> 
     @csrf
-    <input name="nome" type="text" placeholder="Nome" class="{{ $classe }}">
+    <input name="nome" value="{{ old('nome') }}" type="text" placeholder="Nome" class="{{ $classe }}">
     <br>
-    <input name="telefone" type="text" placeholder="Telefone" class="{{ $classe }}">
+    <input name="telefone" value="{{ old('telefone') }}" type="text" placeholder="Telefone" class="{{ $classe }}">
     <br>
-    <input name="email" type="text" placeholder="E-mail" class="{{ $classe }}">
+    <input name="email" value="{{ old('email') }}" type="text" placeholder="E-mail" class="{{ $classe }}">
     <br>
     <select name="motivo_contato" id="motivo_contato" class="{{ $classe }}">
         <option value="">Qual o motivo do contato?</option>
@@ -14,11 +14,11 @@
         <option value="3">Reclamação</option>
     </select>
     <br>
-    <textarea name="mensagem" class="{{ $classe }}">Preencha aqui a sua mensagem</textarea>
+    <textarea name="mensagem" class="{{ $classe }}">{{ (old('mensagem') != '') ?  (old('mensagem')) : 'Preencha aqui a sua mensagem' }} </textarea>
     <br>
     <button type="submit" class="{{ $classe }}">ENVIAR</button>
 </form>
 
-<pre style="position: absolute; top:0px; left:0px; width:100%; background: red;">
+{{-- <pre style="position: absolute; top:0px; left:0px; width:100%; background: red;">
 {{ print_r($errors) }}
-</pre>
+</pre> --}}
