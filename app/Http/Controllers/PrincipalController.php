423b1538ccb095184f95eceb4a1da99a1a18266c
Sylvio Cavalcanti <sylviocavalcanti2020@gmail.com>
2023-01-06 20:16:14 -0300

    Fix: Enviando array motivo_contatos para a view principal

--- app/Http/Controllers/PrincipalController.php
+++ app/Http/Controllers/PrincipalController.php
@@ -7,10 +7,12 @@ use Illuminate\Http\Request;
 class PrincipalController extends Controller
 {
     public function principal() {
-        /*
-            site.principal -> esta acessando o arquivo
-            'principal.blade.php'
-        */
-        return view('site.principal');
+        $motivo_contatos = [
+            '1' => 'Dúvida',
+            '2' => 'Elogio',
+            '3' => 'Reclamação',
+        ];
+        // site.principal -> esta acessando o arquivo 'principal.blade.php'
+        return view('site.principal', ['motivo_contatos' => $motivo_contatos]);
     }
 }
