d33e80a0f34c305d6be35fcb8a57a7fa626fb32c
Sylvio Cavalcanti <sylviocavalcanti2020@gmail.com>
2023-01-06 11:12:40 -0300

    Passando variavel do controller ContatoController para a View Contato, e passando a variavel da View para um componente. Implementando foreach para para renderizar options de um select

--- app/Http/Controllers/ContatoController.php
+++ app/Http/Controllers/ContatoController.php
@@ -9,24 +9,12 @@ class ContatoController extends Controller
 {
     public function contato(Request $request) {
 
-        // $contato = new SiteContato();
-        // $contato->nome = $request->input('nome');
-        // $contato->telefone = $request->input('telefone');
-        // $contato->email = $request->input('email');
-        // $contato->motivo_contato = $request->input('motivo_contato');
-        // $contato->mensagem = $request->input('mensagem');
-        // // print_r($contato->getAttributes());
-        // $contato->save();
-        
-        // $contato = new SiteContato();
-        // $contato->fill($request->all());
-        // print_r($contato->getAttributes());
-        // $contato->save();
-
-        // $contato = new SiteContato();
-        // $contato->create($request->all());
-
-        return view('site.contato', ['titulo' => 'Contato (teste)']);
+        $motivo_contatos = [
+            '1' => 'Dúvida',
+            '2' => 'Elogio',
+            '3' => 'Reclamação',
+        ];
+        return view('site.contato', ['titulo' => 'Contato (teste)', 'motivo_contatos' => $motivo_contatos]);
     }
 
     public function salvar(Request $request) {
@@ -44,3 +32,22 @@ class ContatoController extends Controller
         SiteContato::create($request->all());
     }
 }
+
+
+
+// $contato = new SiteContato();
+// $contato->nome = $request->input('nome');
+// $contato->telefone = $request->input('telefone');
+// $contato->email = $request->input('email');
+// $contato->motivo_contato = $request->input('motivo_contato');
+// $contato->mensagem = $request->input('mensagem');
+// // print_r($contato->getAttributes());
+// $contato->save();
+
+// $contato = new SiteContato();
+// $contato->fill($request->all());
+// print_r($contato->getAttributes());
+// $contato->save();
+
+// $contato = new SiteContato();
+// $contato->create($request->all());
--- resources/views/site/contato.blade.php
+++ resources/views/site/contato.blade.php
@@ -10,7 +10,7 @@
 
         <div class="informacao-pagina">
             <div class="contato-principal">
-                @component('site.layouts._components.form_contato', ['classe' => 'borda-preta'])
+                @component('site.layouts._components.form_contato', ['classe' => 'borda-preta', 'motivo_contatos' => $motivo_contatos ])
                     <p>
                         A nossa equipe analisará a sua mensagem e retornaremos o
                         mais breve possível.
@@ -22,7 +22,6 @@
             </div>
         </div>  
     </div>
-
     <div class="rodape">
         <div class="redes-sociais">
             <h2>Redes sociais</h2>
