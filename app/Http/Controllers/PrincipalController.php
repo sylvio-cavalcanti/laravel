96fd812f752481d40c21e950419c665e272dcbd9
Sylvio Cavalcanti <sylviocavalcanti2020@gmail.com>
2023-01-09 10:32:15 -0300

    Refactorig: recuperando registros da tabela motivo_contatos

--- app/Http/Controllers/PrincipalController.php
+++ app/Http/Controllers/PrincipalController.php
@@ -2,16 +2,14 @@
 
 namespace App\Http\Controllers;
 
+use App\MotivoContato;
 use Illuminate\Http\Request;
 
 class PrincipalController extends Controller
 {
     public function principal() {
-        $motivo_contatos = [
-            '1' => 'Dúvida',
-            '2' => 'Elogio',
-            '3' => 'Reclamação',
-        ];
+        $motivo_contatos = MotivoContato::all();
+        
         // site.principal -> esta acessando o arquivo 'principal.blade.php'
         return view('site.principal', ['motivo_contatos' => $motivo_contatos]);
     }
